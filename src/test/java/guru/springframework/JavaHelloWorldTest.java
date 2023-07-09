1d4e01cc7e93655dfd3645e6ce403bf23fc792ad
graheed <graheed@gmail.com>
2023-07-09 16:23:31 -0500

    Added one JUnit4 testcase

--- src/test/java/guru/springframework/JavaHelloWorldTest.java
+++ src/test/java/guru/springframework/JavaHelloWorldTest.java
@@ -1,8 +1,17 @@
 package guru.springframework;
+import org.junit.*;
+
+import static org.junit.Assert.assertEquals;
 
 public class JavaHelloWorldTest {
     public void testGetHello() {
         JavaHelloWorld javaHelloWorld = new JavaHelloWorld();
         assert("Hello World".equals(javaHelloWorld.getHello()));
+    }
+
+    @Test
+    public void testGetHelloJunit() {
+        JavaHelloWorld javaHelloWorld = new JavaHelloWorld();
+        assertEquals("Hello World", javaHelloWorld.getHello());
     }
 }
