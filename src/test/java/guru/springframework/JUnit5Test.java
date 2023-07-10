d302d640b1ac8a77d09c1cb23e4fef1bd348f9d0
Raheed Gilzene <rgilzene@griddynamics.com>
2023-07-10 10:59:54 -0500

    Added JUnit5, JUnit5 and Spock simple testing support to Maven project

---
+++ src/test/java/guru/springframework/JUnit5Test.java
@@ -0,0 +1,9 @@
+package guru.springframework;
+
+import org.junit.jupiter.api.Test;
+
+public class JUnit5Test {
+    @Test
+    void exampleTest() {
+    }
+}
--- src/test/java/guru/springframework/JavaHelloWorldTest.java
+++ src/test/java/guru/springframework/JavaHelloWorldTest.java
@@ -5,13 +5,17 @@ import static org.junit.Assert.assertEquals;
 
 public class JavaHelloWorldTest {
     public void testGetHello() {
+
         JavaHelloWorld javaHelloWorld = new JavaHelloWorld();
         assert("Hello World".equals(javaHelloWorld.getHello()));
     }
 
     @Test
     public void testGetHelloJunit() {
+
         JavaHelloWorld javaHelloWorld = new JavaHelloWorld();
         assertEquals("Hello World", javaHelloWorld.getHello());
     }
+
+
 }
